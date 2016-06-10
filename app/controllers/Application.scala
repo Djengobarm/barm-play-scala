58262774427795e16a2392f1f14dd43844952208
Barm <djengobarm@gmail.com>
2016-06-11 00:42:09 +0300

    Fixed code style

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -16,66 +16,66 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class Application @Inject() (userRepo: UserRepository, val messagesApi: MessagesApi) (implicit ec: ExecutionContext) extends Controller with I18nSupport {
 
-  val Home = Redirect(routes.Application.index())
+    val Home = Redirect(routes.Application.index())
 
-  val userForm: Form[CreateUserForm] = Form {
-    mapping(
-      "name" -> nonEmptyText
-    )(CreateUserForm.apply)(CreateUserForm.unapply)
-  }
-
-  def index = Action { implicit request =>
-    Ok(views.html.index(userForm))
-  }
-
-  def addUser = Action.async { implicit request =>
-      userForm.bindFromRequest.fold(
-        errorForm => {
-          Future.successful(Ok(views.html.index(errorForm)))
-        },
-        user => {
-          userRepo.add(user.name).map { _ =>
-              Redirect(routes.Application.index())
-          }
-        }
-      )
-  }
-
-  def getUsers = Action.async {
-    userRepo.list().map { users =>
-      Ok(Json.toJson(users))
+    val userForm: Form[CreateUserForm] = Form {
+        mapping(
+            "name" -> nonEmptyText
+        )(CreateUserForm.apply)(CreateUserForm.unapply)
     }
-  }
 
-  def deleteUser(id: Int) = Action.async { implicit request =>
+    def index = Action { implicit request =>
+        Ok(views.html.index(userForm))
+    }
 
-      val futureExists = userRepo.exists(id)
+    def addUser = Action.async { implicit request =>
+        userForm.bindFromRequest.fold(
+            errorForm => {
+                Future.successful(Ok(views.html.index(errorForm)))
+            },
+            user => {
+                userRepo.add(user.name).map { _ =>
+                    Redirect(routes.Application.index())
+                }
+            }
+        )
+    }
 
-      futureExists.map { result => {
-        if ( result ) {
-          userRepo.delete(id)
-          Ok("success")
-          // get future delete
-//          val futureDel = userRepo.delete(id)
-//
-//          val successMessage = "User #" + id.toString + " have been deleted"
-//
-//          // Redirect to home page with Success flash message
-//          futureDel.map { res => Home.flashing("success" -> successMessage) }.recover {
-//            // if anything goes wrong
-//            case ex: TimeoutException =>
-//              Logger("User #" + id.toString + " can not be deleted. " + ex.getMessage)
-//              InternalServerError(ex.getMessage)
-//            case ex: SQLException =>
-//              Logger("User #" + id.toString + " can not be deleted. " + ex.getMessage)
-//              InternalServerError(ex.getMessage)
-//          }
-        } else {
-//          throw new Exception("sdfsdfsdf")
-          Ok("not success")
+    def getUsers = Action.async {
+        userRepo.list().map { users =>
+            Ok(Json.toJson(users))
         }
-      }}
-  }
+    }
+
+    def deleteUser(id: Int) = Action.async { implicit request =>
+
+        val futureExists = userRepo.exists(id)
+
+        futureExists.map { result => {
+            if ( result ) {
+                userRepo.delete(id)
+                Ok("success")
+                // get future delete
+                //          val futureDel = userRepo.delete(id)
+                //
+                //          val successMessage = "User #" + id.toString + " have been deleted"
+                //
+                //          // Redirect to home page with Success flash message
+                //          futureDel.map { res => Home.flashing("success" -> successMessage) }.recover {
+                //            // if anything goes wrong
+                //            case ex: TimeoutException =>
+                //              Logger("User #" + id.toString + " can not be deleted. " + ex.getMessage)
+                //              InternalServerError(ex.getMessage)
+                //            case ex: SQLException =>
+                //              Logger("User #" + id.toString + " can not be deleted. " + ex.getMessage)
+                //              InternalServerError(ex.getMessage)
+                //          }
+            } else {
+                //          throw new Exception("sdfsdfsdf")
+                Ok("not success")
+            }
+        }}
+    }
 
 }
 
